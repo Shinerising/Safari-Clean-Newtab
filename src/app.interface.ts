a8d61d05e49dabd0a5be82d8ddad07ad8554a115
Apollo Wayne <waynezhaoyang@gmail.com>
2021-11-08 16:21:00 +0800

    Create app.interface.ts

---
+++ src/app.interface.ts
@@ -0,0 +1,128 @@
+export interface UnsplashResult {
+  results: UnsplashData[];
+}
+export interface UnsplashData {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  width: number;
+  height: number;
+  color: string;
+  description: string;
+  urls: {
+    raw: string;
+    full: string;
+    regular: string;
+    small: string;
+    thumb: string;
+  };
+  links: {
+    self: string;
+    html: string;
+    download: string;
+    download_location: string;
+  };
+  categories: string[];
+  sponsored: boolean;
+  sponsored_by: string;
+  sponsored_impressions_id: string;
+  likes: number;
+  liked_by_user: boolean;
+  current_user_collections: string[];
+  user: {
+    id: string;
+    updated_at: string;
+    username: string;
+    name: string;
+    first_name: string;
+    last_name: string;
+    twitter_username: string;
+    portfolio_url: string;
+    bio: string;
+    location: string;
+    links: {
+      self: string;
+      html: string;
+      photos: string;
+      likes: string;
+      portfolio: string;
+      following: string;
+      followers: string;
+    };
+    profile_image: {
+      small: string;
+      medium: string;
+      large: string;
+    };
+    instagram_username: string;
+    total_collections: number;
+    total_likes: number;
+    total_photos: number;
+    accepted_tos: boolean;
+  };
+  exif: {
+    make: string;
+    name: string;
+    model: string;
+    exposure_time: string;
+    aperture: string;
+    focal_length: string;
+    iso: number;
+  };
+  location: {
+    title: string;
+    name: string;
+    city: string;
+    country: string;
+    position: {
+      latitude: number;
+      longitude: number;
+    };
+  };
+}
+
+export interface BingDailyImageResult {
+  images: BingDailyImageData[];
+}
+
+export interface BingDailyImageData {
+  startdate: string;
+  fullstartdate: string;
+  enddate: string;
+  url: string;
+  urlbase: string;
+  copyright: string;
+  copyrightlink: string;
+}
+
+export interface AppleSearchData {
+  wrapperType: string;
+  kind: string;
+  artistId: number;
+  collectionId: number;
+  trackId: number;
+  artistName: string;
+  collectionName: string;
+  trackName: string;
+  collectionCensoredName: string;
+  trackCensoredName: string;
+  artistViewUrl: string;
+  collectionViewUrl: string;
+  trackViewUrl: string;
+  previewUrl: string;
+  artworkUrl30: string;
+  artworkUrl60: string;
+  artworkUrl100: string;
+  collectionPrice: number;
+  trackPrice: number;
+  releaseDate: string;
+  collectionExplicitness: string;
+  trackExplicitness: string;
+  discCount: number;
+  discNumber: number;
+  trackCount: number;
+  trackNumber: number;
+  trackTimeMillis: number;
+  country: string;
+  currency: string;
+}
